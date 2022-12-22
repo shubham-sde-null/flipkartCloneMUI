6a63e8004f938e85365bc0b50480904e09f2b760
shubham-sde-null <shubhamsdecpporigin@gmail.com>
2022-12-23 00:23:06 +0530

    new cahnges

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -22,7 +22,24 @@ function CartItems({ src, mobileName, spec1, price, delivery, id }) {
         <div className={isMatch ? "productDescDataSmall" : "productDescData"}>
           <h3 style={{ fontSize: isMatch ? "1rem" : "" }}>{mobileName}</h3>
           <p style={{ fontSize: isMatch ? "0.9rem" : "" }}>{spec1}</p>
-          <h3 style={{ fontSize: isMatch ? "1.2rem" : "" }}>₹{price}</h3>
+          <h3 style={{ fontSize: isMatch ? "1.2rem" : "" }}>
+            ₹
+            {price.toString().length === 4
+              ? `${price.toString()[0]},${price.toString()[1]}${
+                  price.toString()[2]
+                }${price.toString()[3]}`
+              : price.toString().length === 5
+              ? `${price.toString()[0]}${price.toString()[1]},${
+                  price.toString()[2]
+                }${price.toString()[3]}${price.toString()[4]}`
+              : price.toString().length === 6
+              ? `${price.toString()[0]},${price.toString()[1]}${
+                  price.toString()[2]
+                },${price.toString()[3]}${price.toString()[4]}${
+                  price.toString()[5]
+                }`
+              : ``}
+          </h3>
           <h6 style={{ color: "#388e3c", fontSize: "0.8rem" }}>{delivery}</h6>
         </div>
       </div>
